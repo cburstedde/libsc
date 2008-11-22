4ac59323acdc78e3d517622b24e29c2c3ffbfb96
Carsten Burstedde <carsten@ices.utexas.edu>
2008-11-22 12:25:18 -0600

    Make the sc_allgather files GPL as well and update a header.

--- src/sc_allgather.c
+++ src/sc_allgather.c
@@ -1,10 +1,24 @@
 /*
- * Copyright (c) 2008 Carsten Burstedde <carsten@ices.utexas.edu>
- *
- * May only be used with the Mangll, Rhea and P4est codes.
- * Any other use is prohibited.
- */
+  This file is part of the SC Library.
+  The SC library provides support for parallel scientific applications.
 
+  Copyright (C) 2008 Carsten Burstedde, Lucas Wilcox.
+
+  The SC Library is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  The SC Library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with the SC Library.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/* sc.h comes first in every compilation unit */
 #include <sc.h>
 #include <sc_allgather.h>
 
