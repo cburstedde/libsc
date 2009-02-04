22e6a7a61286de9bc22c5bd81364caf066926c18
Carsten Burstedde <carsten@ices.utexas.edu>
2009-02-04 15:34:01 -0600

    Add and export lua header files.

---
+++ src/sc_lua.h
@@ -0,0 +1,50 @@
+/*
+  This file is part of the SC Library.
+  The SC library provides support for parallel scientific applications.
+
+  Copyright (C) 2009 Carsten Burstedde, Lucas Wilcox.
+
+  The SC Library is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  The SC Library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with the SC Library.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef SC_LUA_H
+#define SC_LUA_H
+
+#ifndef SC_H
+#error "sc.h should be included before this header file"
+#endif
+
+#define LUA_USE_LINUX
+
+#ifdef SC_PROVIDE_LUA
+#ifdef lua_h
+#error "lua.h is included.  Include sc.h first or use --without-lua".
+#endif
+#include "sc_builtin/lua.h"
+#include "sc_builtin/lualib.h"
+#include "sc_builtin/lauxlib.h"
+#else
+#ifdef SC_HAVE_LUA_H
+#include <lua.h>
+#include <lualib.h>
+#include <lauxlib.h>
+#endif
+#ifdef SC_HAVE_LUA5_1_LUA_H
+#include <lua5.1/lua.h>
+#include <lua5.1/lualib.h>
+#include <lua5.1/lauxlib.h>
+#endif
+#endif
+
+#endif /* !SC_LUA_H */
