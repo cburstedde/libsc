23b02e24808933df7360b40253ba4e62f32c1062
Carsten Burstedde <carsten@ices.utexas.edu>
2010-02-27 22:05:15 -0600

    Start sc_search files.

---
+++ src/sc_search.h
@@ -0,0 +1,30 @@
+/*
+  This file is part of the SC Library.
+  The SC library provides support for parallel scientific applications.
+
+  Copyright (C) 2010 Carsten Burstedde, Lucas Wilcox.
+
+  The SC Library is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  The SC Library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with the SC Library.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef SC_SEARCH_H
+#define SC_SEARCH_H
+
+#include <sc.h>
+
+SC_EXTERN_C_BEGIN;
+
+SC_EXTERN_C_END;
+
+#endif /* !SC_SEARCH_H */
