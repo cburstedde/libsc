7652714b5ff5191a5366b713eef8e4e7531fe64d
Carsten Burstedde <carsten@ices.utexas.edu>
2010-05-11 10:56:24 -0500

    Remove code from libgfortran.

--- src/sc_c99_functions.h
+++ src/sc_c99_functions.h
@@ -1,31 +1,24 @@
-/* Declarations of various C99 functions
-   Copyright (C) 2004, 2006, 2007 Free Software Foundation, Inc.
+/*
+  This file is part of the SC Library.
+  The SC Library provides support for parallel scientific applications.
 
-This file is part of the GNU Fortran 95 runtime library (libgfortran).
+  Copyright (C) 2010 The University of Texas System
 
-Libgfortran is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation; either
-version 2.1 of the License, or (at your option) any later version.
+  The SC Library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
 
-Libgfortran is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Lesser General Public License for more details.
+  The SC Library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
 
-You should have received a copy of the GNU Lesser General Public
-License along with libgfortran; see the file COPYING.LIB.  If not,
-write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-Boston, MA 02110-1301, USA.  */
-
-/* As a special exception, if you link this library with other files,
-   some of which are compiled with GCC, to produce an executable,
-   this library does not by itself cause the resulting executable
-   to be covered by the GNU General Public License.
-   This exception does not however invalidate any other reasons why
-   the executable file might be covered by the GNU General Public License.  */
-
-/* made some changes to preprocessor macros for libsc */
+  You should have received a copy of the GNU Lesser General Public
+  License along with the SC Library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+  02110-1301 USA.
+*/
 
 #ifndef SC_C99_FUNCTIONS_H
 #define SC_C99_FUNCTIONS_H
@@ -41,10 +34,6 @@ int                 fileno (FILE * stream);
 FILE               *fopen (const char *path, const char *mode);
 FILE               *fdopen (int fd, const char *mode);
 int                 mkstemp (char *template);
-
-/* Gamma-related prototypes */
-double              tgamma (double);
-double              trunc (double);
 
 /* Long size integer support */
 intmax_t            imaxabs (intmax_t);
