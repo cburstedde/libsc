435740c1847f37e7914700f6b994f27f4b80a1b3
Carsten Burstedde <carsten@ices.utexas.edu>
2010-12-23 23:40:48 +0100

    Implement some polynom functions

--- src/sc_polynom.c
+++ src/sc_polynom.c
@@ -21,3 +21,93 @@
 */
 
 #include <sc_polynom.h>
+
+sc_polynom_t       *
+sc_polynom_new (void)
+{
+  const double        zero = 0.;
+
+  return sc_polynom_new_from_coefficients (0, &zero);
+}
+
+void
+sc_polynom_destroy (sc_polynom_t * p)
+{
+  SC_ASSERT (p->degree < (int) p->c->elem_count);
+
+  sc_array_destroy (p->c);
+  SC_FREE (p);
+}
+
+sc_polynom_t       *
+sc_polynom_new_from_coefficients (int degree, const double *coefficients)
+{
+  int                 i;
+  double             *d;
+  sc_polynom_t       *p;
+
+  SC_ASSERT (degree >= 0);
+
+  p = SC_ALLOC (sc_polynom_t, 1);
+  p->c = sc_array_new_size (sizeof (double), (size_t) degree + 1);
+
+  for (i = 0; i <= degree; ++i) {
+    d = (double *) sc_array_index_int (p->c, i);
+    *d = coefficients[i];
+  }
+
+  return p;
+}
+
+void
+sc_polynom_set_degree (sc_polynom_t * p, int degree)
+{
+  int                 i;
+
+  SC_ASSERT (degree >= 0);
+
+#ifdef SC_DEBUG
+  for (i = degree; i < p->degree; ++i) {
+    *((double *) sc_array_index_int (p->c, i + 1)) = 0. / 0.;
+  }
+#endif
+  sc_array_resize (p->c, (size_t) degree + 1);
+  for (i = p->degree; i < degree; ++i) {
+    *((double *) sc_array_index_int (p->c, i + 1)) = 0.;
+  }
+  p->degree = degree;
+}
+
+void
+sc_polynom_set_value (sc_polynom_t * p, double value)
+{
+  sc_polynom_set_degree (p, 0);
+
+  *((double *) sc_array_index (p->c, 0)) = value;
+}
+
+void
+sc_polynom_scale (sc_polynom_t * p, int exponent, double factor)
+{
+  const int           degree = p->degree;
+  int                 i;
+
+  SC_ASSERT (exponent >= 0);
+
+  if (exponent == 0) {
+    for (i = 0; i <= degree; ++i) {
+      *((double *) sc_array_index_int (p->c, i)) *= factor;
+    }
+  }
+  else {
+    sc_polynom_set_degree (p, degree + exponent);
+
+    for (i = degree; i >= 0; --i) {
+      *((double *) sc_array_index_int (p->c, i + exponent)) =
+        factor * *((double *) sc_array_index_int (p->c, i));
+    }
+    for (i = 0; i < exponent; ++i) {
+      *((double *) sc_array_index_int (p->c, i)) = 0.;
+    }
+  }
+}
--- src/sc_polynom.h
+++ src/sc_polynom.h
@@ -25,4 +25,62 @@
 
 #include <sc_containers.h>
 
+SC_EXTERN_C_BEGIN;
+
+/* Data structure and basic constructor and destructor */
+
+typedef struct sc_polynom
+{
+  int                 degree;   /* Degree of polynom sum_i=0^degree c_i x^i */
+  sc_array_t         *c;        /* Array of type double stores coefficients.
+                                   It holds degree < c->elem_count. */
+}
+sc_polynom_t;
+
+/** Create the zero polynom */
+sc_polynom_t       *sc_polynom_new (void);
+
+/** Destroy all memory used by a polynom */
+void                sc_polynom_destroy (sc_polynom_t * p);
+
+/* Alternate constructors */
+
+/** Create a polynom from given monomial coefficients.
+ * \param[in] degree            Degree of the polynom, >= 0.
+ * \param[in] coefficients      Monomial coefficients [0..degree].
+ */
+sc_polynom_t       *sc_polynom_new_from_coefficients (int degree,
+                                                      const double
+                                                      *coefficients);
+sc_polynom_t       *sc_polynom_new_from_lagrange (int degree, int which,
+                                                  const double *points);
+
+/* Alternate constructors using other polynoms */
+
+sc_polynom_t       *sc_polynom_new_from_scale (sc_polynom_t * a,
+                                               int exponent, double factor);
+sc_polynom_t       *sc_polynom_new_from_sum (sc_polynom_t * a,
+                                             sc_polynom_t * b);
+sc_polynom_t       *sc_polynom_new_from_product (sc_polynom_t * a,
+                                                 sc_polynom_t * b);
+
+/* Manipulating a polynom */
+
+/** Set degree of a polynomial.
+ * If the new degree is larger than the old degree, the new coefficients
+ * are set to zero.  If the new degree is smaller, the smaller set of
+ * coefficients is unchanged.
+ */
+void                sc_polynom_set_degree (sc_polynom_t * p, int degree);
+void                sc_polynom_set_value (sc_polynom_t * p, double value);
+
+/** Scale a polynom by a monomial.
+ * \param[in] exponent  Exponent of the monomial, >= 0.
+ * \param[in] factor    Prefactor of the monomial.
+ */
+void                sc_polynom_scale (sc_polynom_t * p,
+                                      int exponent, double factor);
+
+SC_EXTERN_C_END;
+
 #endif /* !SC_POLYNOM_H */
