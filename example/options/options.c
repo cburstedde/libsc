7cbb9dc45acfdef61c7518b9783d5c183c21ffd0
Adrian Seyboldt <adrian.seyboldt@gmail.com>
2012-09-12 14:41:23 +0200

    update examples to use sc_MPI

--- example/options/options.c
+++ example/options/options.c
@@ -48,13 +48,13 @@ main (int argc, char **argv)
   const char         *cd = "Callback example";
   sc_options_t       *opt, *subopt;
 
-  mpiret = MPI_Init (&argc, &argv);
+  mpiret = sc_MPI_Init (&argc, &argv);
   SC_CHECK_MPI (mpiret);
 
-  mpiret = MPI_Comm_rank (MPI_COMM_WORLD, &rank);
+  mpiret = sc_MPI_Comm_rank (sc_MPI_COMM_WORLD, &rank);
   SC_CHECK_MPI (mpiret);
 
-  sc_init (MPI_COMM_WORLD, 1, 1, NULL, SC_LP_DEFAULT);
+  sc_init (sc_MPI_COMM_WORLD, 1, 1, NULL, SC_LP_DEFAULT);
 
   opt = sc_options_new (argv[0]);
   sc_options_add_switch (opt, 'w', "switch", &w, "Switch");
@@ -120,7 +120,7 @@ main (int argc, char **argv)
 
   sc_finalize ();
 
-  mpiret = MPI_Finalize ();
+  mpiret = sc_MPI_Finalize ();
   SC_CHECK_MPI (mpiret);
 
   return 0;
