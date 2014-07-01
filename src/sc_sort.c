a0fc6fbce87b5cd64650eb4b36ee9ea20a01d1a5
Carsten Burstedde <burstedde@ins.uni-bonn.de>
2014-07-01 14:40:30 +0200

    Close an issue on sc_sort

--- src/sc_sort.c
+++ src/sc_sort.c
@@ -415,10 +415,6 @@ sc_psort (sc_MPI_Comm mpicomm, void *base, size_t * nmemb, size_t size,
 
   SC_ASSERT (sc_compare == NULL);
 
-#ifndef SC_DEBUG
-  SC_ABORT ("sc_psort is still buggy, don't use it yet");
-#endif
-
   /* get basic MPI information */
   mpiret = sc_MPI_Comm_size (mpicomm, &num_procs);
   SC_CHECK_MPI (mpiret);
