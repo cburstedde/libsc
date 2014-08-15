848f0f163321a4c968c2da485b7be5f9595aa675
Johann Rudi <johann@ices.utexas.edu>
2014-08-15 10:49:45 -0500

    sc_blas/lapack: BG/Q fix of naming convention for blas/lapack functions.

--- src/sc_lapack.h
+++ src/sc_lapack.h
@@ -40,8 +40,12 @@ extern const char   sc_jobzchar[];
 #ifdef SC_WITH_LAPACK
 
 #ifndef SC_F77_FUNC
+#ifdef __bgq__
+#define SC_F77_FUNC(small,CAPS) small
+#else
 #define SC_F77_FUNC(small,CAPS) small ## _
 #endif
+#endif /* SC_F77_FUNC */
 
 #define SC_LAPACK_DGELS   SC_F77_FUNC(dgels,DGELS)
 #define SC_LAPACK_DGETRF  SC_F77_FUNC(dgetrf,DGETRF)
