6ea4eefc4561d0214631df96b8a09799872718ae
Johann Rudi <johann@ices.utexas.edu>
2015-01-28 18:08:35 -0600

    sc: Modify output.

--- src/sc.c
+++ src/sc.c
@@ -243,7 +243,7 @@ sc_calloc (int package, size_t nmemb, size_t size)
   ret = calloc (nmemb, size);
 
   if (nmemb * size > 0) {
-    SC_CHECK_ABORTF (ret != NULL, "Allocation (calloc size %lli",
+    SC_CHECK_ABORTF (ret != NULL, "Allocation (calloc size %lli)",
                      (long long int) size);
     ++*malloc_count;
   }
@@ -268,7 +268,7 @@ sc_realloc (int package, void *ptr, size_t size)
     void               *ret;
 
     ret = realloc (ptr, size);
-    SC_CHECK_ABORTF (ret != NULL, "Reallocation (realloc size %lli",
+    SC_CHECK_ABORTF (ret != NULL, "Reallocation (realloc size %lli)",
                      (long long int) size);
 
     return ret;
