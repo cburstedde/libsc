fe07af58e0daf1320d80085eda1da3d4959833ab
Carsten Burstedde <burstedde@ins.uni-bonn.de>
2019-11-21 19:43:11 +0100

    Begin stubs for sc3 error handling

---
+++ src/sc3_error.h
@@ -0,0 +1,48 @@
+/*
+  This file is part of the SC Library, version 3+.
+  The SC Library provides support for parallel scientific applications.
+
+  Copyright (C) 2019 individual authors
+
+  The SC Library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  The SC Library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with the SC Library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+  02110-1301, USA.
+*/
+
+/** \file sc3_error.h
+ */
+
+#ifndef SC3_ERROR_H
+#define SC3_ERROR_H
+
+#include <sc3.h>
+
+#ifdef __cplusplus
+extern              "C"
+{
+#if 0
+}
+#endif
+#endif
+
+typedef struct sc3_error sc3_error_t;
+
+#ifdef __cplusplus
+#if 0
+{
+#endif
+}
+#endif
+
+#endif /* !SC3_ERROR_H */
