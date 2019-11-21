e870cde0ac4fe9314f747a63d0db2fd238f8d6c6
Carsten Burstedde <burstedde@ins.uni-bonn.de>
2019-11-21 20:12:25 +0100

    Work on sc3_error API

--- src/sc3_error.h
+++ src/sc3_error.h
@@ -37,6 +37,54 @@ extern              "C"
 #endif
 
 typedef struct sc3_error sc3_error_t;
+typedef struct sc3_error_args sc3_error_args_t;
+
+typedef enum sc3_error_severity
+{
+  SC3_RUNTIME,
+  SC3_WARNING,
+  SC3_FATAL,
+  SC3_ERROR_SEVERITY_LAST
+}
+sc3_error_severity_t;
+
+typedef enum sc3_error_sync
+{
+  SC3_LOCAL,
+  SC3_SYNCED,
+  SC3_DISAGREE,
+  SC3_ERROR_SYNC_LAST
+}
+sc3_error_sync_t;
+
+/*** TODO pass counting memory allocator to constructor */
+
+/*** TODO implement reference counting */
+
+sc3_error_args_t   *sc3_error_args_new (void);
+void                sc3_error_args_set_from (sc3_error_args_t * ea,
+                                             sc3_error_t from);
+void                sc3_error_args_set_severity (sc3_error_args_t * ea,
+                                                 sc3_error_severity_t sev);
+void                sc3_error_args_set_sync (sc3_error_args_t * ea,
+                                             sc3_error_sync_t syn);
+void                sc3_error_args_set_msg (sc3_error_args_t * ea,
+                                            const char *errmsg);
+void                sc3_error_args_set_msgf (sc3_error_args_t * ea,
+                                             const char *errfmt, ...);
+void                sc3_error_args_destroy (sc3_error_args_t * ea);
+
+sc3_error_t        *sc3_error_new (sc3_error_args_t ea);
+
+sc3_error_t        *sc3_error_new_new (sc3_error_severity_t sev,
+                                       sc3_error_sync_t syn,
+                                       const char *errmsg);
+sc3_error_t        *sc3_error_new_from (sc3_error_t * from,
+                                        const char *errmsg);
+
+/*** TODO need a bunch of _get_ and/or _is_ functions ***/
+
+void                sc3_error_destroy (sc3_error_t * e);
 
 #ifdef __cplusplus
 #if 0
