f1f68dc611489403985eed85132b7b705ee0c10d
Carsten Burstedde <burstedde@ins.uni-bonn.de>
2020-03-20 17:55:54 +0100

    Work on documentation of sc3

--- src/sc3_base.h
+++ src/sc3_base.h
@@ -28,6 +28,67 @@
 */
 
 /** \defgroup sc3 sc3
+ * Version 3 of the sc library is a rewrite.
+ *
+ * The main goals compared to previous versions are to improve the error
+ * handling and to remove any global/static variables.
+ * In particular, functions do no longer call `MPI_Abort()` on failed
+ * assertions but return error objects.
+ * These error objects can be returned all the way up to the toplevel code.
+ * We have also introduced RAII objects and reference counting.
+ * Memory allocation is wrapped into allocators that keep internal counts.
+ * We do not provide any locking, thus it is recommended to create one
+ * allocator per thread, and generally to work with a hierarchy of allocators.
+ * We provide various data containers, for example variable-length arrays.
+ *
+ * There is no `sc3.h` file to include everything.
+ * To just pull in the configuration and system headers, include \ref
+ * sc3_base.h.
+ * We removed the global initialization and finalize functions.
+ * There is no more global state to the library.
+ *
+ * We use a minimal reference counting mechanism declared in
+ * \ref sc3_refcount.h and \ref sc3_refcount_internal.h.
+ * (Internal header files are not part of the public API but allow access to
+ * lower levels of the code.)  While the \ref sc3_refcount_t object is public,
+ * most objects in sc3 are opaque and manipulated by API functions only.
+ *
+ * Allocators are declared in \ref sc3_alloc.h, and errors in \ref sc3_error.h.
+ * These two files depend on each other and, combined with the reference
+ * counters, constitute the basis of the code.
+ *
+ * Containers are declared in \ref sc3_array.h.
+ *
+ * The design we provide is not meant to reinvent object oriented programming.
+ * It is a set of minimal conventions that establish a consistent call pattern
+ * of the code.
+ * - An object begins with its life with the `sc3_object_new` call and a
+ *   reference count of 1.
+ *   These functions usually take an allocator input parameter that is
+ *   used to allocate the object and all future resources inside of it.
+ * - After the `new` call, properties of the object may be specified by
+ *   `sc3_object_set_*` functions.
+ *   It is fine to call the same set function multiple times or to temporarily
+ *   set conflicting values.
+ *   In this stage, the object cannot yet be referenced, but it can be
+ *   unreferenced and destroyed (which amount to the same result here).
+ * - After setting a consistent set of parameters, call `sc3_object_setup`.
+ *   This creates a usable object that can no longer be reparameterized,
+ *   thus it is immutable and safe to be referenced and passed to other code.
+ *   There may be exceptions to immutability, for example an array may be
+ *   resized after setup, but the size of each element may not be changed.
+ * - When removing an object's last reference with `sc3_object_unref`, it is
+ *   deallocated together with all its internal resources.
+ *   The `sc3_object_destroy` functions must only be called if the program
+ *   ensures that at this point the reference count is 1.
+ * - The `sc3_object_is_*` functions may be called on any pointer to this
+ *   object type including `NULL`.
+ *   They query properties of the object that are false if the pointer is
+ *   `NULL`.
+ *   One convention is to provide for `sc3_object_is_new`,
+ *   `sc3_object_is_setup` and `sc3_object_is_valid`.
+ *   The first two refer to the mutually exclusive phases of its life,
+ *   while the third may be used any time to verify internal consistency.
  */
 
 /** \file sc3_base.h
