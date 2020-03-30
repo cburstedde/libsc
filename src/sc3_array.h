4fde8ac69df7a9119537fd63182ee42688913fec
Carsten Burstedde <burstedde@ins.uni-bonn.de>
2020-03-30 12:17:54 +0200

    Update sc3_array doc

--- src/sc3_array.h
+++ src/sc3_array.h
@@ -36,15 +36,15 @@
  * The array can serve as a push-and-pop stack.
  *
  * We use standard `int` types for indexing.
- * The total array size may be greater than 2GB depending on the
+ * The total array memory size may be greater than 2GB depending on the
  * size of each element.
  *
  * In the setup phase, we set the size of the array and optionally
  * an inital count, an initzero property, and some more.
  * The caller may also decide during setup if the array is resizable.
  * After setup, the array may be resized while it is resizable.
- * Resizability can be terminated by calling \ref sc3_array_freeze.
- * The freeze is not reversible.
+ * Resizability can be terminated by calling \ref sc3_array_freeze
+ * (only after \sc3_array_setup).  The freeze is not reversible.
  *
  * An array can only be refd if it is setup and non-resizable.
  * Otherwise, the usual ref-, unref- and destroy semantics hold.
@@ -209,7 +209,9 @@ sc3_error_t        *sc3_array_ref (sc3_array_t * a);
 sc3_error_t        *sc3_array_unref (sc3_array_t ** ap);
 
 /** Destroy an array with a reference count of one.
- * It is an error to destroy an array that is multiply referenced.
+ * It is a leak error to destroy an array that is multiply referenced.
+ * We unref its internal allocator, which may cause a fatal error if
+ * that allocator has been used against specification elsewhere in the code.
  * \param [in,out] ap   This array must be valid and have a refcount of 1.
  *                      On output, value is set to NULL.
  * \return              NULL on success, error object otherwise.
