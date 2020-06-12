93053319024951b6e039640a1a41afaf416e9703
Carsten Burstedde <burstedde@ins.uni-bonn.de>
2020-06-12 14:42:12 +0200

    Begin v3 log example

---
+++ example/v3basics/log.c
@@ -0,0 +1,29 @@
+/*
+  This file is part of the SC Library, version 3.
+  The SC Library provides support for parallel scientific applications.
+
+  Copyright (C) 2019 individual authors
+
+  The SC Library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  The SC Library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with the SC Library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+  02110-1301, USA.
+*/
+
+#include <sc3_log.h>
+
+int
+main (int argc, char ** argv)
+{
+  return 0;
+}
