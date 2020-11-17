293e3184f540b5c807d1ca15eb4d263839937848
Hendrik Ranocha <mail@ranocha.de>
2020-11-17 20:20:59 +0100

    add functions returning the full/major/minor version of libsc

--- src/sc.c
+++ src/sc.c
@@ -1508,4 +1508,22 @@ sc_snprintf (char *str, size_t size, const char *fmt, ...)
   va_end (ap);
 }
 
+const char*
+sc_version (void)
+{
+  return SC_VERSION;
+}
+
+int
+sc_version_major (void)
+{
+  return SC_VERSION_MAJOR;
+}
+
+int
+sc_version_minor (void)
+{
+  return SC_VERSION_MINOR;
+}
+
 #endif
