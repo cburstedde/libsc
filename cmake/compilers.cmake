c79c96efb8ce68f1cd2211a46613d6ca9c7eaf19
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2021-02-12 14:17:34 -0500

    enhance cmake packaging

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -1,3 +1,10 @@
+# avoid extremely long error printouts as it's ususally the first error that's relevant
+set(_maxerr 3)
+
 if(CMAKE_C_COMPILER_ID STREQUAL GNU)
-  add_compile_options(-fmax-errors=3)  # avoid extremely long error printouts
+  add_compile_options(-fmax-errors=${_maxerr})
+elseif(CMAKE_C_COMPILER_ID STREQUAL Intel)
+  add_compile_options(-diag-error-limit=${_maxerr})
+elseif(CMAKE_C_COMPILER_ID STREQUAL IntelLLVM)
+  add_compile_options(-fmax-errors=${_maxerr})
 endif()
