44a309a3a20a33ca0e715d44a4d7e60b0a17edcd
Michael Hirsch <scivision@users.noreply.github.com>
2021-02-24 09:28:20 -0500

    cmake default install build/.local

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -3,3 +3,10 @@ option(openmp "use OpenMP" on)
 
 
 set(CMAKE_EXPORT_COMPILE_COMMANDS on)
+
+# --- default install directory under build/local
+# users can specify like "cmake -B build -DCMAKE_INSTALL_PREFIX=~/mydir"
+if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
+  # will not take effect without FORCE
+  set(CMAKE_INSTALL_PREFIX "${PROJECT_BINARY_DIR}/local" CACHE PATH "Install top-level directory" FORCE)
+endif()
