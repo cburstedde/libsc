f1fc5ff962ebd082cde396c16ea7aa4f27d7f11d
Carsten Burstedde <burstedde@ins.uni-bonn.de>
2021-06-20 18:36:21 +0200

    Optionally build with debug and MPI

--- doc/nix/builder.sh
+++ doc/nix/builder.sh
@@ -1,9 +1,23 @@
 
 # setup environment
 source $stdenv/setup
-export PATH=$which/bin:$gnum4/bin:$autoconf/bin:$automake/bin:$libtool/bin:$pkgconf/bin:$mpich/bin:$PATH
+export PATH=$which/bin:$gnum4/bin:$autoconf/bin:$automake/bin:$libtool/bin:$pkgconf/bin:$PATH
 export ACLOCAL_PATH=$pkgconf/share/aclocal:$libtool/share/aclocal
+
+if test "x$debugEnable" = x1 ; then
+echo "Enabling debug build"
+CONFIG_ENABLE_DEBUG="--enable-debug"
+CFLAGS_EXTRA="-O -g"
+else
+CFLAGS_EXTRA="-O2"
+fi
+
+if test "x$mpiSupport" = x1 ; then
+echo "Enabling MPI support"
+export PATH=$mpi/bin:$PATH
 export CC=mpicc
+CONFIG_ENABLE_MPI="--enable-mpi"
+fi
 
 # initialize source
 cp -r $src ./fromgit
@@ -14,8 +28,8 @@ chmod -R u+w ./fromgit
 mkdir build && cd build
 ../fromgit/configure \
   CPPFLAGS="-I$zlibdev/include" \
-  CFLAGS="-O -g -Wall" \
+  CFLAGS="$CFLAGS_EXTRA -Wall" \
   LDFLAGS="-L$zlib/lib" \
-  --enable-debug --enable-mpi --prefix=$out
+  $CONFIG_ENABLE_DEBUG $CONFIG_ENABLE_MPI --prefix=$out
 make -j8 V=0
 make -j8 V=0 install
--- doc/nix/default.nix
+++ doc/nix/default.nix
@@ -1,9 +1,11 @@
 {
   stdenv, fetchgit,
+  which, gnum4, autoconf, automake, libtool, pkgconf,
   debugEnable ? true, mpiSupport ? true,
-  which, gnum4, autoconf, automake, libtool, pkgconf, mpich, zlib
+  mpich ? null, zlib
 }:
 
+assert mpiSupport -> mpich != null;
 
 stdenv.mkDerivation {
   name = "p4est-sc-prev3-develop-1ae814e3";
@@ -14,6 +16,8 @@ stdenv.mkDerivation {
     rev = "1ae814e3fb1cc5456652e0d77550386842cb9bfb";
     sha256 = "14vm0b162jh8399pgpsikbwq4z5lkrw9vfzy3drqykw09n6nc53z";
   };
-  inherit which gnum4 autoconf automake libtool pkgconf mpich zlib;
+  inherit which gnum4 autoconf automake libtool pkgconf zlib;
+  inherit debugEnable mpiSupport;
+  mpi = if mpiSupport then mpich else null;
   zlibdev = zlib.dev;
 }
