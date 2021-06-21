f2da18ae26e7b6004fab445fee07e04b15c25af3
Carsten Burstedde <burstedde@ins.uni-bonn.de>
2021-06-21 10:45:20 +0200

    Add maintainers attribute

--- doc/nix/default.nix
+++ doc/nix/default.nix
@@ -1,5 +1,5 @@
 {
-  stdenv, fetchgit,
+  stdenv, fetchgit, lib,
   which, gnum4, autoconf, automake, libtool, pkgconf,
   debugEnable ? true, mpiSupport ? true,
   mpich ? null, zlib
@@ -16,8 +16,17 @@ stdenv.mkDerivation {
     rev = "1ae814e3fb1cc5456652e0d77550386842cb9bfb";
     sha256 = "14vm0b162jh8399pgpsikbwq4z5lkrw9vfzy3drqykw09n6nc53z";
   };
+
   inherit which gnum4 autoconf automake libtool pkgconf zlib;
   inherit debugEnable mpiSupport;
   mpi = if mpiSupport then mpich else null;
   zlibdev = zlib.dev;
+
+  meta = {
+    branch = "prev3-develop";
+    description = "Support parallel scientific applications";
+    homepage = https://www.p4est.org/;
+    licence = lib.licenses.lgpl21Plus;
+    maintainers = [ "Carsten Burstedde <burstedde@ins.uni-bonn.de>" ];
+  };
 }
