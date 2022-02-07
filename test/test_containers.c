4c3b0d9c193d09c56f94a75d42d9c70da31b2895
Carsten Burstedde <burstedde@ins.uni-bonn.de>
2022-02-07 22:07:04 +0100

    sc3 test_containers: update with latest conventions

--- test/test_containers.c
+++ test/test_containers.c
@@ -27,10 +27,22 @@
   POSSIBILITY OF SUCH DAMAGE.
 */
 
-#include <sc.h>
-#include <sc3_memstamp.h>
-#include <sc3_array.h>
 #include <sc_random.h>
+#include <sc3_array.h>
+#include <sc3_log.h>
+#include <sc3_memstamp.h>
+
+/** If a condition \a x is not met, return an assertion error.
+ * Its message is set to the failed condition and argument \a s.
+ *
+ * Do we like this macro?  Shall we include it in sc3_error.h?
+ */
+#define SC3E_CONDITION(x,s) do {                                        \
+  if (!(x)) {                                                           \
+    char _errmsg[SC3_BUFSIZE];                                          \
+    sc3_snprintf (_errmsg, SC3_BUFSIZE, "%s: %s", s, #x);               \
+    return sc3_error_new_assert (__FILE__, __LINE__, _errmsg);          \
+  }} while (0)
 
 static sc3_error_t *
 test_allocations (void)
@@ -39,23 +51,35 @@ test_allocations (void)
   const size_t        msize[3] = { 20, 37, 537 };
   int                 i, j;
   size_t              isize;
-  void               *pc;
+  char               *pc, **backup;
   sc3_mstamp_t       *mst;
+  sc3_allocator_t    *alloc;
+  sc3_array_t        *items;
+
+  SC3E (sc3_allocator_new (NULL, &alloc));
+  SC3E (sc3_allocator_set_align (alloc, 32));
+  SC3E (sc3_allocator_setup (alloc));
+
+  SC3E (sc3_array_new (alloc, &items));
+  SC3E (sc3_array_set_elem_size (items, sizeof (char *)));
+  SC3E (sc3_array_setup (items));
 
   for (j = 0; j < 3; ++j) {
+
     /* zero data size */
-    SC3E (sc3_mstamp_new (sc3_allocator_nocount (), &mst));
+    SC3E (sc3_mstamp_new (alloc, &mst));
     SC3E (sc3_mstamp_set_stamp_size (mst, munit[j]));
     SC3E (sc3_mstamp_set_elem_size (mst, 0));
     SC3E (sc3_mstamp_setup (mst));
     for (i = 0; i < 8 + 3 * j; ++i) {
       SC3E (sc3_mstamp_alloc (mst, &pc));
-      SC_CHECK_ABORT (pc == NULL, "Mstamp alloc NULL");
+      SC3E_CONDITION (pc == NULL, "Mstamp alloc NULL");
     }
     SC3E (sc3_mstamp_destroy (&mst));
+
     /* proper data size */
     isize = msize[j];
-    SC3E (sc3_mstamp_new (sc3_allocator_nocount (), &mst));
+    SC3E (sc3_mstamp_new (alloc, &mst));
     SC3E (sc3_mstamp_set_stamp_size (mst, munit[j]));
     SC3E (sc3_mstamp_set_elem_size (mst, isize));
     SC3E (sc3_mstamp_setup (mst));
@@ -65,23 +89,33 @@ test_allocations (void)
     }
     SC3E (sc3_mstamp_destroy (&mst));
 
-    SC3E (sc3_mstamp_new (sc3_allocator_nocount (), &mst));
+    /* allocate, free, and allocate again */
+    SC3E (sc3_mstamp_new (alloc, &mst));
     SC3E (sc3_mstamp_set_stamp_size (mst, munit[j]));
     SC3E (sc3_mstamp_set_elem_size (mst, isize));
     SC3E (sc3_mstamp_setup (mst));
     for (i = 0; i < 3124; ++i) {
       SC3E (sc3_mstamp_alloc (mst, &pc));
       memset (pc, -1, isize);
+      SC3E (sc3_array_push (items, &backup));
+      *backup = pc;
     }
     for (i = 0; i < 3124; ++i) {
-      SC3E (sc3_mstamp_free (mst, pc));
+      SC3E (sc3_array_index (items, i, &backup));
+      pc = *backup;
+      SC3E (sc3_mstamp_free (mst, &pc));
     }
     for (i = 0; i < 3124; ++i) {
       SC3E (sc3_mstamp_alloc (mst, &pc));
       memset (pc, -1, isize);
     }
+    SC3E (sc3_array_resize (items, 0));
     SC3E (sc3_mstamp_destroy (&mst));
   }
+
+  SC3E (sc3_array_destroy (&items));
+  SC3E (sc3_allocator_destroy (&alloc));
+
   return NULL;
 }
 
@@ -89,7 +123,6 @@ static sc3_error_t *
 test_correctness (void)
 {
   const int           nelems = 7829;
-  const int           shift = 1;
   const int           per_stamp = 3;
   int                 i, ecount;
   size_t              isize;
@@ -99,7 +132,7 @@ test_correctness (void)
 
   isize = sizeof (long);
 
-  SC3E (sc3_mstamp_new (sc3_allocator_nocount (), &mst));
+  SC3E (sc3_mstamp_new (NULL, &mst));
   SC3E (sc3_mstamp_set_stamp_size (mst, per_stamp * isize + 1));
   SC3E (sc3_mstamp_set_elem_size (mst, isize));
   SC3E (sc3_mstamp_set_initzero (mst, 1));
@@ -107,29 +140,31 @@ test_correctness (void)
   for (i = 0; i < nelems; ++i) {
     SC3E (sc3_mstamp_alloc (mst, &pc));
     tv = (long *) pc;
-    SC3E_DEMAND (*tv == 0L, "initzero doesn't work");
+    SC3E_CONDITION (*tv == 0L, "initzero doesn't work");
     *tv = 42L;
-    SC3E_DEMAND (*(long *) pc == 42L, "wrong stamp access");
+    SC3E_CONDITION (*(long *) pc == 42L, "wrong stamp access");
   }
   SC3E (sc3_mstamp_get_elem_count (mst, &ecount));
-  SC3E_DEMAND (ecount == nelems, "wrong number of elements");
+  SC3E_CONDITION (ecount == nelems, "wrong number of elements");
 
+#if 0
+  /* pc needs to be a separate valid item in each iteration */
   for (i = 0; i < nelems - shift; ++i) {
-    SC3E (sc3_mstamp_free (mst, pc));
-    *(long *) pc = 21L;
+    SC3E (sc3_mstamp_free (mst, &pc));
   }
   SC3E (sc3_mstamp_get_elem_count (mst, &ecount));
-  SC3E_DEMAND (ecount == shift, "wrong number of elements after freeing");
+  SC3E_CONDITION (ecount == shift, "wrong number of elements after freeing");
+#endif
 
   for (i = 0; i < nelems; ++i) {
     SC3E (sc3_mstamp_alloc (mst, &pc));
     tv = (long *) pc;
-    SC3E_DEMAND (*tv == 0L, "initzero doesn't work after freeing");
+    SC3E_CONDITION (*tv == 0L, "initzero doesn't work after freeing");
     *tv = 42L;
-    SC3E_DEMAND (*(long *) pc == 42L, "wrong stamp access after freeing");
+    SC3E_CONDITION (*(long *) pc == 42L, "wrong stamp access after freeing");
   }
   SC3E (sc3_mstamp_get_elem_count (mst, &ecount));
-  SC3E_DEMAND (ecount == nelems + shift, "wrong number of elements");
+  SC3E_CONDITION (ecount == 2 * nelems, "wrong number of elements");
   SC3E (sc3_mstamp_destroy (&mst));
 
   return NULL;
@@ -150,7 +185,7 @@ test_view (void)
   sc3_allocator_t    *alloc;
 
   /* create a toplevel allocator */
-  SC3E (sc3_allocator_new (sc3_allocator_nocount (), &alloc));
+  SC3E (sc3_allocator_new (NULL, &alloc));
   SC3E (sc3_allocator_setup (alloc));
 
   /*create and fill a simple sc3_array and c-array */
@@ -171,15 +206,15 @@ test_view (void)
   for (i = 0; i < length; ++i) {
     SC3E (sc3_array_index (a, i + offset, &ptr));
     SC3E (sc3_array_index (view, i, &ptr_view));
-    SC3E_DEMAND (*(int *) ptr == *(int *) ptr_view && ptr == ptr_view,
-                 "the view points to the wrong memory");
+    SC3E_CONDITION (*(int *) ptr == *(int *) ptr_view && ptr == ptr_view,
+                    "the view points to the wrong memory");
   }
-  SC3E (sc3_array_renew_view (&view, a, offset / 2, length / 2));
+  SC3E (sc3_array_renew_view (view, a, offset / 2, length / 2));
   for (i = 0; i < length / 2; ++i) {
     SC3E (sc3_array_index (a, i + offset / 2, &ptr));
     SC3E (sc3_array_index (view, i, &ptr_view));
-    SC3E_DEMAND (*(int *) ptr == *(int *) ptr_view && ptr == ptr_view,
-                 "the view points to the wrong memory");
+    SC3E_CONDITION (*(int *) ptr == *(int *) ptr_view && ptr == ptr_view,
+                    "the view points to the wrong memory");
   }
 
   SC3E (sc3_array_destroy (&view));
@@ -189,82 +224,45 @@ test_view (void)
   SC3E (sc3_array_new_data (alloc, &view, data, isize, offset, length));
   for (i = 0; i < length; ++i) {
     SC3E (sc3_array_index (view, i, &ptr_view));
-    SC3E_DEMAND (data[i + offset] == *(int *) ptr_view,
-                 "the view points to the wrong memory");
+    SC3E_CONDITION (data[i + offset] == *(int *) ptr_view,
+                    "the view points to the wrong memory");
   }
-  SC3E (sc3_array_renew_data (&view, data, isize, offset / 2, length / 2));
+  SC3E (sc3_array_renew_data (view, data, isize, offset / 2, length / 2));
   for (i = 0; i < length / 2; ++i) {
     SC3E (sc3_array_index (view, i, &ptr_view));
-    SC3E_DEMAND (data[i + offset / 2] == *(int *) ptr_view,
-                 "the view points to the wrong memory");
+    SC3E_CONDITION (data[i + offset / 2] == *(int *) ptr_view,
+                    "the view points to the wrong memory");
   }
   /* renew view with a NULL data */
-  SC3E (sc3_array_renew_data (&view, NULL, isize, offset, 0));
+  SC3E (sc3_array_renew_data (view, NULL, isize, offset, 0));
 
   /*destroy the view and free the data */
-  SC3E (sc3_allocator_free (alloc, data));
+  SC3E (sc3_allocator_free (alloc, &data));
   SC3E (sc3_array_destroy (&view));
   SC3E (sc3_allocator_destroy (&alloc));
   return NULL;
 }
 
 static sc3_error_t *
-output_error (sc3_error_t ** pe)
+test_all (void)
 {
-  int                 eline;
-  int                 depth;
-  const char         *efile;
-  const char         *emsg;
-  sc3_error_kind_t    ekind;
-  sc3_error_t        *e, *s;
-
-  SC3E_DEMAND (pe != NULL && *pe != NULL, "Misuse of output_error");
-  depth = 0;
-  e = *pe;
-  *pe = NULL;
-  while (e != NULL) {
-    SC3E (sc3_error_access_location (e, &efile, &eline));
-    SC3E (sc3_error_access_message (e, &emsg));
-    SC3E (sc3_error_get_kind (e, &ekind));
-
-    fprintf (stderr, "Error %d %s:%d %c: %s\n",
-             depth, efile, eline, sc3_error_kind_char[ekind], emsg);
-
-    SC3E (sc3_error_restore_location (e, efile, eline));
-    SC3E (sc3_error_restore_message (e, emsg));
-    SC3E (sc3_error_ref_stack (e, &s));
-    SC3E (sc3_error_destroy (&e));
-    e = s;
-    ++depth;
-  }
+  SC3E (test_allocations ());
+  SC3E (test_correctness ());
+  SC3E (test_view ());
   return NULL;
-}
-
-static void
-report_error (sc3_error_t ** pe)
-{
-  sc3_error_t        *e;
-
-  if (pe != NULL && *pe != NULL) {
-    e = output_error (pe);
-    if (e != NULL || pe == NULL || *pe != NULL) {
-      /* This error should never occur.  Something is wrong on the inside. */
-      SC_ABORT ("Internal error inconsistency\n");
-    }
-    /* We reached some error before this function that has now been printed. */
-    SC_ABORT ("sc3 container tests failed\n");
-  }
 }
 
 int
 main (int argc, char **argv)
 {
-  sc3_error_t        *e;
-  e = test_allocations ();
-  report_error (&e);
-  e = test_correctness ();
-  report_error (&e);
-  e = test_view ();
-  report_error (&e);
-  return 0;
+  sc3_MPI_Comm_t      mpicomm = SC3_MPI_COMM_WORLD;
+  int                 mpirank;
+
+  SC3X (sc3_MPI_Init (&argc, &argv));
+  SC3X (sc3_MPI_Comm_rank (mpicomm, &mpirank));
+  if (mpirank == 0) {
+    SC3X (test_all ());
+  }
+  SC3X (sc3_MPI_Finalize ());
+  return EXIT_SUCCESS;
 }
