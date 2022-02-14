74b4e521debc3fa2ece16234408c6fb85c4d36e4
Carsten Burstedde <burstedde@ins.uni-bonn.de>
2022-02-14 23:10:31 +0100

    sc3_options: more code and example

---
+++ example/sc3basic/options.c
@@ -0,0 +1,73 @@
+/*
+  This file is part of the SC Library, version 3.
+  The SC Library provides support for parallel scientific applications.
+
+  Copyright (C) 2019 individual authors
+
+  The SC Library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  The SC Library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with the SC Library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+  02110-1301, USA.
+*/
+
+#include <sc3_log.h>
+#include <sc3_options.h>
+
+typedef struct options_global
+{
+  sc3_allocator_t    *alloc;
+  int                 i1;
+  int                 i2;
+}
+options_global_t;
+
+static sc3_error_t *
+parse_options (options_global_t * g, int argc, char **argv)
+{
+  sc3_options_t      *opt;
+
+  SC3E (sc3_options_new (g->alloc, &opt));
+  SC3E (sc3_options_add_int (opt, 'i', "--int-one", "First integer",
+                             &g->i1, 6));
+  SC3E (sc3_options_add_int (opt, 'j', "--int-two", "Second integer",
+                             &g->i2, 7));
+  SC3E (sc3_options_setup (opt));
+
+  /* parse options */
+
+  SC3E (sc3_options_destroy (&opt));
+  return NULL;
+}
+
+static sc3_error_t *
+run_main (options_global_t * g, int argc, char **argv)
+{
+  SC3E (sc3_allocator_new (NULL, &g->alloc));
+  SC3E (sc3_allocator_set_align (g->alloc, 0));
+  SC3E (sc3_allocator_setup (g->alloc));
+
+  SC3E (parse_options (g, argc, argv));
+
+  SC3E (sc3_allocator_destroy (&g->alloc));
+  return NULL;
+}
+
+int
+main (int argc, char **argv)
+{
+  options_global_t global, *g = &global;
+  SC3X (sc3_MPI_Init (&argc, &argv));
+  SC3X (run_main (g, argc, argv));
+  SC3X (sc3_MPI_Finalize ());
+  return EXIT_SUCCESS;
+}
--- src/sc3_options.c
+++ src/sc3_options.c
@@ -27,8 +27,30 @@
   POSSIBILITY OF SUCH DAMAGE.
 */
 
+#include <sc3_array.h>
 #include <sc3_options.h>
 #include <sc3_refcount.h>
+
+typedef enum sc3_option_type
+{
+  SC3_OPTION_INT,
+  SC3_OPTION_TYPE_LAST
+}
+sc3_option_type_t;
+
+typedef struct sc3_option
+{
+  sc3_option_type_t   opt_type;
+  int                 opt_short;
+  const char         *opt_long;
+  size_t              opt_long_len;
+  const char         *opt_help;
+  union
+  {
+    int                *var_int;
+  } v;
+}
+sc3_option_t;
 
 struct sc3_options
 {
@@ -36,6 +58,9 @@ struct sc3_options
   sc3_refcount_t      rc;
   sc3_allocator_t    *alloc;
   int                 setup;
+
+  /* internal configuration */
+  sc3_array_t        *opts;
 
 #if 0
   /* parameters set before and fixed after setup */
@@ -53,14 +78,16 @@ sc3_options_is_valid (const sc3_options_t * yy, char *reason)
   SC3E_IS (sc3_refcount_is_valid, &yy->rc, reason);
   SC3E_IS (sc3_allocator_is_setup, yy->alloc, reason);
 
-#if 0
+  /* check options array */
   if (!yy->setup) {
-    SC3E_TEST (yy->member == NULL, reason);
+    SC3E_IS (sc3_array_is_resizable, yy->opts, reason);
   }
   else {
-    SC3E_TEST (yy->member != NULL && *yy->member == yy->dummy, reason);
+    SC3E_IS (sc3_array_is_unresizable, yy->opts, reason);
   }
-#endif
+
+  /* go through individial options */
+
   SC3E_YES (reason);
 }
 
@@ -99,17 +126,26 @@ sc3_options_new (sc3_allocator_t * alloc, sc3_options_t ** yyp)
 
   SC3E_RETVAL (yyp, NULL);
 
+  /* allocator to be used */
   if (alloc == NULL) {
     alloc = sc3_allocator_new_static ();
   }
   SC3A_IS (sc3_allocator_is_setup, alloc);
 
+  /* internal metadata */
   SC3E (sc3_allocator_ref (alloc));
   SC3E (sc3_allocator_calloc (alloc, 1, sizeof (sc3_options_t), &yy));
   SC3E (sc3_refcount_init (&yy->rc));
   yy->alloc = alloc;
-  SC3A_IS (sc3_options_is_new, yy);
 
+  /* internal array of options */
+  SC3E (sc3_array_new (alloc, &yy->opts));
+  SC3E (sc3_array_set_elem_size (yy->opts, sizeof (sc3_option_t)));
+  SC3E (sc3_array_set_tighten (yy->opts, 1));
+  SC3E (sc3_array_setup (yy->opts));
+
+  /* done with allocation */
+  SC3A_IS (sc3_options_is_new, yy);
   *yyp = yy;
   return NULL;
 }
@@ -127,15 +163,33 @@ sc3_options_set_dummy (sc3_options_t * yy, int dummy)
 #endif
 
 sc3_error_t        *
+sc3_options_add_int (sc3_options_t * yy,
+                     int opt_short, const char *opt_long,
+                     const char *opt_help, int *opt_variable, int opt_value)
+{
+  sc3_option_t       *o;
+
+  SC3A_IS (sc3_options_is_new, yy);
+  SC3A_CHECK (opt_variable != NULL);
+
+  SC3E (sc3_array_push (yy->opts, &o));
+  o->opt_type = SC3_OPTION_INT;
+  o->opt_short = opt_short;
+  o->opt_long = opt_long;
+  o->opt_long_len = strlen (opt_long);
+  o->opt_help = opt_help;
+  *(o->v.var_int = opt_variable) = opt_value;
+
+  return NULL;
+}
+
+sc3_error_t        *
 sc3_options_setup (sc3_options_t * yy)
 {
   SC3A_IS (sc3_options_is_new, yy);
 
-#if 0
-  /* allocate internal state */
-  SC3E (sc3_allocator_malloc (yy->alloc, sizeof (int), &yy->member));
-  *yy->member = yy->dummy;
-#endif
+  /* finalize internal state */
+  SC3E (sc3_array_freeze (yy->opts));
 
   /* done with setup */
   yy->setup = 1;
@@ -163,14 +217,14 @@ sc3_options_unref (sc3_options_t ** yyp)
   SC3E (sc3_refcount_unref (&yy->rc, &waslast));
   if (waslast) {
     *yyp = NULL;
-
     alloc = yy->alloc;
+
+    /* deallocate internal state */
     if (yy->setup) {
-#if 0
-      /* deallocate internal state */
-      SC3E (sc3_allocator_free (alloc, &yy->member));
-#endif
     }
+    SC3E (sc3_array_destroy (&yy->opts));
+
+    /* deallocate array itself */
     SC3E (sc3_allocator_free (alloc, &yy));
     SC3E (sc3_allocator_unref (&alloc));
   }
@@ -209,8 +263,9 @@ sc3_options_parse (sc3_options_t * yy, int argc, const char **argv,
                    int *arg_pos, int *result)
 {
   SC3E_RETVAL (result, -1);
-  SC3A_IS (sc3_options_is_valid, yy);
+  SC3A_IS (sc3_options_is_setup, yy);
   SC3A_CHECK (0 <= argc);
+  SC3A_CHECK (argv != NULL);
   SC3A_CHECK (arg_pos != NULL);
   SC3A_CHECK (0 <= *arg_pos && *arg_pos < argc);
 
