5e0b8f353b81b2206a3ed29121e007357962f3ac
Michael Hirsch <scivision@users.noreply.github.com>
2022-02-18 09:30:21 -0500

    cmake: shared lib options for packaging/reuse

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -3,6 +3,14 @@ option(openmp "use OpenMP" off)
 
 set(CMAKE_EXPORT_COMPILE_COMMANDS on)
 
+# Rpath options necessary for shared library install to work correctly in user projects
+set(CMAKE_INSTALL_NAME_DIR ${CMAKE_INSTALL_PREFIX}/lib)
+set(CMAKE_INSTALL_RPATH ${CMAKE_INSTALL_PREFIX}/lib)
+set(CMAKE_INSTALL_RPATH_USE_LINK_PATH true)
+
+# Necessary for shared library with Visual Studio / Windows oneAPI
+set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS true)
+
 # --- default install directory under build/local
 # users can specify like "cmake -B build --install-prefix=$HOME/mydir"
 if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
