ef585ed6dea8b3fc319e08b87ed23f9c6aef2ed4
Gautam Ghosh <ghosh@ins.uni-bonn.de>
2022-08-02 17:44:55 +0200

    debug commit

--- test/test_sort.c
+++ test/test_sort.c
@@ -48,7 +48,7 @@ main (int argc, char **argv)
   SC_CHECK_MPI (mpiret);
   mpiret = sc_MPI_Comm_rank (mpicomm, &rank);
   SC_CHECK_MPI (mpiret);
-
+  SC_ASSERT(2 == 4);
   sc_init (mpicomm, 1, 1, NULL, SC_LP_DEFAULT);
 
   /*
