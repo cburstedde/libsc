d4807b8beeef7ad163b772db5648e0a8dbf96dcc
scivision <scivision@users.noreply.github.com>
2023-01-19 10:18:08 -0500

    cmake:pkgconf: specify requirements only if used
    fixes #107

--- cmake/pkgconf.cmake
+++ cmake/pkgconf.cmake
@@ -1,5 +1,10 @@
 # --- generate pkg-config .pc
-set(pc_req_private "ompi ompi-c orte zlib")
+set(pc_req_private "")
+if(mpi)
+  string(APPEND pc_req_private " ompi ompi-c zlib")
+elseif(zlib)
+  string(APPEND pc_req_private " zlib")
+endif()
 
 set(pc_filename libsc-${git_version}.pc)
 configure_file(${CMAKE_CURRENT_LIST_DIR}/pkgconf.pc.in ${pc_filename} @ONLY)
