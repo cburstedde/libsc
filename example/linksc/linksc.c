02bd7dc079c7d8289bc07f2071565a0c33fb481e
Carsten Burstedde <burstedde@ins.uni-bonn.de>
2023-04-03 21:27:03 +0200

    Feature linksc: make example compile under MPI

--- example/linksc/linksc.c
+++ example/linksc/linksc.c
@@ -4,8 +4,16 @@
 int
 main (int argc, char **argv)
 {
+  int                 mpiret;
+
+  mpiret = sc_MPI_Init (&argc, &argv);
+  SC_CHECK_MPI (mpiret);
+
   sc_init (sc_MPI_COMM_WORLD, 0, 0, NULL, SC_LP_DEFAULT);
   sc_finalize ();
+
+  mpiret = sc_MPI_Finalize ();
+  SC_CHECK_MPI (mpiret);
 
   return EXIT_SUCCESS;
 }
