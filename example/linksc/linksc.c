6d81c37168dbc19cf91b8923d89b2d611bcc3f56
Carsten Burstedde <burstedde@ins.uni-bonn.de>
2023-04-03 21:13:37 +0200

    Feature linksc: try building a demo program

--- example/linksc/linksc.c
+++ example/linksc/linksc.c
@@ -1,8 +1,11 @@
 
-#include <stdio.h>
+#include <sc.h>
 
 int
 main (int argc, char **argv)
 {
+  sc_init (sc_MPI_COMM_WORLD, 0, 0, NULL, SC_LP_DEFAULT);
+  sc_finalize ();
+
   return EXIT_SUCCESS;
 }
