0b42ef4585bb37ff68a2178533b45f4f5293682c
Pierre Kestener <pierre.kestener@cea.fr>
2023-04-22 11:50:13 +0200

    Add libsc.pc as a symlink to the installed pkgconfig file.
    trying to fix issue https://github.com/cburstedde/p4est/issues/202

--- cmake/pkgconf.cmake
+++ cmake/pkgconf.cmake
@@ -11,3 +11,13 @@ configure_file(${CMAKE_CURRENT_LIST_DIR}/pkgconf.pc.in ${pc_filename} @ONLY)
 
 install(FILES ${CMAKE_CURRENT_BINARY_DIR}/${pc_filename}
         DESTINATION lib/pkgconfig)
+
+set(pc_target ${pc_filename})
+set(pc_link ${CMAKE_INSTALL_PREFIX}/lib/pkgconfig/libsc.pc)
+
+install(CODE "execute_process( \
+    COMMAND ${CMAKE_COMMAND} -E create_symlink \
+    ${pc_target} \
+    ${pc_link}   \
+    )"
+  )
