19f953999ddcb6ea698e3c94b8a7bff30897b1fd
Pierre Kestener <pierre.kestener@cea.fr>
2023-04-27 11:43:42 +0200

    Sanitize script to prevent from non defined variable.

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -5,20 +5,32 @@
 # macro to convert a boolean variable (ON/OFF) to 0/1.
 #
 macro(convert_01 varin varout)
+
+  if(NOT DEFINED ${varin})
+    message(FATAL_ERROR "variable ${varin} not defined")
+  endif()
+
   if(${varin})
     set(${varout} 1)
   else()
     set(${varout} 0)
   endif()
+
 endmacro()
 
 #
 # macro to convert a boolean variable (ON/OFF) to yes/no.
 #
 macro(convert_yn varin varout)
+
+  if(NOT DEFINED ${varin})
+    message(FATAL_ERROR "variable ${varin} not defined")
+  endif()
+
   if(${varin})
     set(${varout} yes)
   else()
     set(${varout} no)
   endif()
+
 endmacro()
