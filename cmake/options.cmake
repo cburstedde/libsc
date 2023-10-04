705c155e4c28a6899515ec0f73fd60c60eb2e350
scivision <scivision@users.noreply.github.com>
2023-10-04 16:44:14 -0400

    options.cmake: default Rpath is better
    HPC or other environments loading modules work better
    with runtime Rpath. Better to use CMake defaults.

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -14,15 +14,11 @@ if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
   set(CMAKE_INSTALL_PREFIX "${PROJECT_BINARY_DIR}/local" CACHE PATH "Install top-level directory" FORCE)
 endif()
 
-# Rpath options necessary for shared library install to work correctly in user projects
-set(CMAKE_INSTALL_NAME_DIR ${CMAKE_INSTALL_FULL_LIBDIR})
-set(CMAKE_INSTALL_RPATH ${CMAKE_INSTALL_FULL_LIBDIR})
-set(CMAKE_INSTALL_RPATH_USE_LINK_PATH true)
 
 # Necessary for shared library with Visual Studio / Windows oneAPI
 set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS true)
 
 # --- auto-ignore build directory
-if(NOT EXISTS ${PROJECT_BINARY_DIR}/.gitignore)
+if(NOT CMAKE_SOURCE_DIR STREQUAL CMAKE_BINARY_DIR)
   file(WRITE ${PROJECT_BINARY_DIR}/.gitignore "*")
 endif()
