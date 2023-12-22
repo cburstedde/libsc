197d4f11dd09c2dbbd0023e3f6129cbde1cea6b5
Michael Hirsch <scivision@users.noreply.github.com>
2023-12-21 20:36:04 -0500

    cmake: ensure proper PIE flags set

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -56,6 +56,8 @@ endif()
 
 # Build all targets with -fPIC so that libsc itself can be linked as a
 # shared library, or linked into a shared library.
+include(CheckPIESupported)
+check_pie_supported()
 set(CMAKE_POSITION_INDEPENDENT_CODE ON)
 
 # --- generate sc_config.h
