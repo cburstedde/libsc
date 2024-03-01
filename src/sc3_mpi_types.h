f7036fccf8aed7dfe0345994732ae36506d608c4
Tim Griesbach <tim.griesbach@uni-bonn.de>
2024-03-01 15:56:29 +0100

    Reintroduce explicit enum offsets

--- src/sc3_mpi_types.h
+++ src/sc3_mpi_types.h
@@ -201,7 +201,7 @@ sc3_MPI_Op_t;
 typedef enum sc3_MPI_Errorcode
 {
   SC3_MPI_SUCCESS = 0,  /**< An MPI function has exited successfully. */
-  SC3_MPI_ERR_ARG ,     /**< An MPI function encountered invalid arguments. */
+  SC3_MPI_ERR_ARG = 14000, /**< An MPI function encountered invalid arguments. */
   SC3_MPI_ERR_COUNT,     /**< An MPI function encountered an invalid count argument. */
   SC3_MPI_ERR_UNKNOWN,  /**< An MPI function has produced an unknown error. */
   SC3_MPI_ERR_OTHER,    /**< An MPI function has produced some known error. */
