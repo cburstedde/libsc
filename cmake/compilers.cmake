8c10fc4c63903492c3b3693762400e1a46f12b31
Alex Dutka <97711898+dutkalex@users.noreply.github.com>
2024-04-25 11:54:32 +0200

    remove useless files

--- cmake/compilers.cmake
+++
@@ -1,9 +0,0 @@
-if(MSVC)
-  add_compile_options(/W4)
-else()
-  add_compile_options(-Wall
-  $<$<COMPILE_LANG_AND_ID:C,AppleClang,Clang>:-Wno-unused-but-set-variable>)
-endif()
-
-# disable nuisance warnings from Visual Studio
-add_compile_definitions($<$<BOOL:${MSVC}>:_CRT_SECURE_NO_WARNINGS>)
--- cmake/config.cmake
+++ cmake/config.cmake
@@ -4,6 +4,30 @@ include(CheckTypeSize)
 include(CheckPrototypeDefinition)
 include(CheckCSourceCompiles)
 
+
+option( SC_ENABLE_MPI "use MPI library" OFF )
+option( SC_ENABLE_OPENMP "use OpenMP" OFF )
+
+option( SC_USE_INTERNAL_ZLIB "build ZLIB" OFF )
+option( SC_USE_INTERNAL_JSON "build Jansson" OFF )
+
+option( SC_BUILD_SHARED_LIBS "build shared libsc" OFF )
+option( SC_BUILD_TESTING "build libsc self-tests" ON )
+option( SC_TEST_WITH_VALGRIND "run self-tests with valgrind" OFF )
+
+
+set_property(DIRECTORY PROPERTY EP_UPDATE_DISCONNECTED true)
+
+# Necessary for shared library with Visual Studio / Windows oneAPI
+set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS true)
+
+# --- auto-ignore build directory
+if(NOT PROJECT_SOURCE_DIR STREQUAL PROJECT_BINARY_DIR)
+  file(GENERATE OUTPUT .gitignore CONTENT "*")
+endif()
+
+
+
 # --- retrieve library interface version from configuration file
 file(STRINGS config/sc_soversion.in SC_SOVERSION_READ
              REGEX "^[ \t]*SC_SOVERSION *= *[0-9:]+")
@@ -249,3 +273,14 @@ check_symbol_exists("SC_ENABLE_MPIIO" ${PROJECT_BINARY_DIR}/include/sc_config.h 
 #   message(NOTICE "This configuration is DEPRECATED and will be disallowed in the future.")
 #   message(NOTICE "If the MPI File API is not available, please disable MPI altogether.")
 # endif()
+
+
+if(MSVC)
+  add_compile_options(/W4)
+else()
+  add_compile_options(-Wall
+  $<$<COMPILE_LANG_AND_ID:C,AppleClang,Clang>:-Wno-unused-but-set-variable>)
+endif()
+
+# disable nuisance warnings from Visual Studio
+add_compile_definitions($<$<BOOL:${MSVC}>:_CRT_SECURE_NO_WARNINGS>)
