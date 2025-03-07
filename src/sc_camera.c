e013b8eab7443452b9c3ccc2d0e057a0701913b8
Ole <s6olwelt@uni-bonn.de>
2025-03-07 19:03:59 +0100

    WIP : First implementations of some camera functions

---
+++ src/sc_camera.c
@@ -0,0 +1,580 @@
+#include <math.h>
+
+#include <sc_camera.h>
+
+/* hier erstmal das ganze mathe zeug */
+
+/* matrix multiplikation */ /* could make size variable (TODO: add static) */
+void sc_camera_mult_4x4_v4(const sc_camera_mat4x4_t mat, 
+    const sc_camera_vec4_t vec, sc_camera_vec4_t out)
+{
+    sc_camera_vec4_t result;
+    for (size_t i = 0; i < 4; ++i)
+    {
+        result[i] = 0.0;
+
+        for (size_t j = 0; j < 4; ++j)
+        {
+            result[i] += mat[i + 4 * j] * vec[j];
+        }
+    }
+
+    memcpy(out, result, 4 * sizeof(sc_camera_coords_t));
+}
+
+static void sc_camera_mult_v4_4x4(const sc_camera_vec4_t vec, const sc_camera_mat4x4_t mat,
+    sc_camera_vec4_t out)
+{
+    sc_camera_vec4_t result;
+    for (size_t i = 0; i < 4; ++i)
+    {
+        result[i] = 0.0;
+
+        for (size_t j = 0; j < 4; ++j)
+        {
+            result[i] += mat[j + 4 * i] * vec[j];
+        }
+    }
+
+    memcpy(out, result, 4 * sizeof(sc_camera_coords_t));
+}
+
+/* Copy paste */
+static int sc_camera_gluInvertMatrix(const sc_camera_mat4x4_t m, 
+    sc_camera_mat4x4_t invOut)
+{
+    sc_camera_mat4x4_t inv;
+    sc_camera_coords_t det;
+    size_t i;
+
+    inv[0] = m[5]  * m[10] * m[15] - 
+             m[5]  * m[11] * m[14] - 
+             m[9]  * m[6]  * m[15] + 
+             m[9]  * m[7]  * m[14] +
+             m[13] * m[6]  * m[11] - 
+             m[13] * m[7]  * m[10];
+
+    inv[4] = -m[4]  * m[10] * m[15] + 
+              m[4]  * m[11] * m[14] + 
+              m[8]  * m[6]  * m[15] - 
+              m[8]  * m[7]  * m[14] - 
+              m[12] * m[6]  * m[11] + 
+              m[12] * m[7]  * m[10];
+
+    inv[8] = m[4]  * m[9] * m[15] - 
+             m[4]  * m[11] * m[13] - 
+             m[8]  * m[5] * m[15] + 
+             m[8]  * m[7] * m[13] + 
+             m[12] * m[5] * m[11] - 
+             m[12] * m[7] * m[9];
+
+    inv[12] = -m[4]  * m[9] * m[14] + 
+               m[4]  * m[10] * m[13] +
+               m[8]  * m[5] * m[14] - 
+               m[8]  * m[6] * m[13] - 
+               m[12] * m[5] * m[10] + 
+               m[12] * m[6] * m[9];
+
+    inv[1] = -m[1]  * m[10] * m[15] + 
+              m[1]  * m[11] * m[14] + 
+              m[9]  * m[2] * m[15] - 
+              m[9]  * m[3] * m[14] - 
+              m[13] * m[2] * m[11] + 
+              m[13] * m[3] * m[10];
+
+    inv[5] = m[0]  * m[10] * m[15] - 
+             m[0]  * m[11] * m[14] - 
+             m[8]  * m[2] * m[15] + 
+             m[8]  * m[3] * m[14] + 
+             m[12] * m[2] * m[11] - 
+             m[12] * m[3] * m[10];
+
+    inv[9] = -m[0]  * m[9] * m[15] + 
+              m[0]  * m[11] * m[13] + 
+              m[8]  * m[1] * m[15] - 
+              m[8]  * m[3] * m[13] - 
+              m[12] * m[1] * m[11] + 
+              m[12] * m[3] * m[9];
+
+    inv[13] = m[0]  * m[9] * m[14] - 
+              m[0]  * m[10] * m[13] - 
+              m[8]  * m[1] * m[14] + 
+              m[8]  * m[2] * m[13] + 
+              m[12] * m[1] * m[10] - 
+              m[12] * m[2] * m[9];
+
+    inv[2] = m[1]  * m[6] * m[15] - 
+             m[1]  * m[7] * m[14] - 
+             m[5]  * m[2] * m[15] + 
+             m[5]  * m[3] * m[14] + 
+             m[13] * m[2] * m[7] - 
+             m[13] * m[3] * m[6];
+
+    inv[6] = -m[0]  * m[6] * m[15] + 
+              m[0]  * m[7] * m[14] + 
+              m[4]  * m[2] * m[15] - 
+              m[4]  * m[3] * m[14] - 
+              m[12] * m[2] * m[7] + 
+              m[12] * m[3] * m[6];
+
+    inv[10] = m[0]  * m[5] * m[15] - 
+              m[0]  * m[7] * m[13] - 
+              m[4]  * m[1] * m[15] + 
+              m[4]  * m[3] * m[13] + 
+              m[12] * m[1] * m[7] - 
+              m[12] * m[3] * m[5];
+
+    inv[14] = -m[0]  * m[5] * m[14] + 
+               m[0]  * m[6] * m[13] + 
+               m[4]  * m[1] * m[14] - 
+               m[4]  * m[2] * m[13] - 
+               m[12] * m[1] * m[6] + 
+               m[12] * m[2] * m[5];
+
+    inv[3] = -m[1] * m[6] * m[11] + 
+              m[1] * m[7] * m[10] + 
+              m[5] * m[2] * m[11] - 
+              m[5] * m[3] * m[10] - 
+              m[9] * m[2] * m[7] + 
+              m[9] * m[3] * m[6];
+
+    inv[7] = m[0] * m[6] * m[11] - 
+             m[0] * m[7] * m[10] - 
+             m[4] * m[2] * m[11] + 
+             m[4] * m[3] * m[10] + 
+             m[8] * m[2] * m[7] - 
+             m[8] * m[3] * m[6];
+
+    inv[11] = -m[0] * m[5] * m[11] + 
+               m[0] * m[7] * m[9] + 
+               m[4] * m[1] * m[11] - 
+               m[4] * m[3] * m[9] - 
+               m[8] * m[1] * m[7] + 
+               m[8] * m[3] * m[5];
+
+    inv[15] = m[0] * m[5] * m[10] - 
+              m[0] * m[6] * m[9] - 
+              m[4] * m[1] * m[10] + 
+              m[4] * m[2] * m[9] + 
+              m[8] * m[1] * m[6] - 
+              m[8] * m[2] * m[5];
+
+    det = m[0] * inv[0] + m[1] * inv[4] + m[2] * inv[8] + m[3] * inv[12];
+
+    if (det == 0)
+        return 0;
+
+    det = 1.0 / det;
+
+    for (i = 0; i < 16; i++)
+        invOut[i] = inv[i] * det;
+
+    return 1;
+}
+
+/* out = A * B */
+static void sc_camera_mult_4x4_4x4(const sc_camera_mat4x4_t A, const sc_camera_mat4x4_t B, 
+    sc_camera_mat4x4_t out)
+{
+    sc_camera_mat4x4_t product;
+    /* i index of rows of product */
+    for (size_t i = 0; i < 4; ++i)
+    {
+        /* j index of columns of product */
+        for (size_t j = 0; j < 4; ++j)
+        {
+            product[i + j * 4] = 0.0;
+            for (size_t k = 0; k < 4; ++k)
+            {
+                product[i + 4 * j] += A[i + 4 * k] * B[k + 4 * j];
+            }
+        }
+    }
+
+    memcpy(out, product, 16 * sizeof(sc_camera_coords_t));
+}
+
+/* cross product out = a x b*/
+static inline void sc_camera_cross_prod(const sc_camera_vec3_t a, 
+    const sc_camera_vec3_t b, sc_camera_vec3_t out)
+{
+    out[0] = a[1] * b[2] - a[2] * b[1];
+    out[1] = a[2] * b[0] - a[0] * b[2];
+    out[2] = a[0] * b[1] - a[1] * b[0];
+}
+
+static inline sc_camera_coords_t sc_camera_norm(const sc_camera_vec3_t x)
+{
+    /* this assumes coord_t = double so solution with macro or wrapper function is needed*/
+    return sqrt(x[0] * x[0] + x[1] * x[1] + x[2] * x[2]);
+}
+
+/* scalar out = alpha * x */
+static inline void sc_camera_scalar(sc_camera_coords_t alpha, 
+    const sc_camera_vec3_t x, sc_camera_vec3_t out)
+{
+    out[0] = alpha * x[0];
+    out[1] = alpha * x[1];
+    out[2] = alpha * x[2];
+}
+
+/* out =  a + b */
+static inline void sc_camera_add(const sc_camera_vec3_t a, 
+    const sc_camera_vec3_t b, sc_camera_vec3_t out)
+{
+    out[0] = a[0] + b[0];
+    out[1] = a[1] + b[1];
+    out[2] = a[2] + b[2];
+}
+
+/* out =  a - b */
+static inline void sc_camera_subtract(const sc_camera_vec3_t a, 
+    const sc_camera_vec3_t b, sc_camera_vec3_t out)
+{
+    out[0] = a[0] - b[0];
+    out[1] = a[1] - b[1];
+    out[2] = a[2] - b[2];
+}
+
+// TODO : remove
+static void print_mat(const sc_camera_coords_t *mat, size_t n)
+{
+    for (size_t i = 0; i < n; i++)
+    {
+        for (size_t j = 0; j < n; j++)
+        {
+            printf("%f ", mat[i + n * j]);
+        }
+        printf("\n");
+    }
+}
+
+static void print_vec(const sc_camera_coords_t *vec, size_t size)
+{
+    for (size_t i = 0; i < size; i++)
+    {
+        printf("%f ", vec[i]);
+    }
+    printf("\n");
+}
+
+
+/* TODO : the sqrt function used is only for double */
+/* the code is from http://www.euclideanspace.com/maths/geometry/rotations/conversions/matrixToQuaternion/
+    with some modifications to fit in this context */
+/* matrix to quaternion */
+static inline void sc_camera_mat3x3_to_quaternion(const sc_camera_mat3x3_t a, 
+    sc_camera_vec4_t q)
+{
+    sc_camera_coords_t trace = a[0 + 3*0] + a[1 + 3*1] + a[2 + 3*2];
+    if( trace > 0 ) {
+        sc_camera_coords_t s = 0.5 / sqrt(trace + 1.0);
+        q[3] = 0.25 / s;
+        q[0] = ( a[2 + 3*1] - a[1 + 3*2] ) * s;
+        q[1] = ( a[0 + 3*2] - a[2 + 3*0] ) * s;
+        q[2] = ( a[1 + 3*0] - a[0 + 3*1] ) * s;
+    } else {
+        if ( a[0 + 3*0] > a[1 + 3*1] && a[0 + 3*0] > a[2 + 3*2] ) {
+        sc_camera_coords_t s = 2.0 * sqrt( 1.0 + a[0 + 3*0] - a[1 + 3*1] - a[2 + 3*2]);
+        q[3] = (a[2 + 3*1] - a[1 + 3*2] ) / s;
+        q[0] = 0.25 * s;
+        q[1] = (a[0 + 3*1] + a[1 + 3*0] ) / s;
+        q[2] = (a[0 + 3*2] + a[2 + 3*0] ) / s;
+        } else if (a[1 + 3*1] > a[2 + 3*2]) {
+        sc_camera_coords_t s = 2.0 * sqrt( 1.0 + a[1 + 3*1] - a[0 + 3*0] - a[2 + 3*2]);
+        q[3] = (a[0 + 3*2] - a[2 + 3*0] ) / s;
+        q[0] = (a[0 + 3*1] + a[1 + 3*0] ) / s;
+        q[1] = 0.25 * s;
+        q[2] = (a[1 + 3*2] + a[2 + 3*1] ) / s;
+        } else {
+        sc_camera_coords_t s = 2.0 * sqrt( 1.0 + a[2 + 3*2] - a[0 + 3*0] - a[1 + 3*1] );
+        q[3] = (a[1 + 3*0] - a[0 + 3*1] ) / s;
+        q[0] = (a[0 + 3*2] + a[2 + 3*0] ) / s;
+        q[1] = (a[1 + 3*2] + a[2 + 3*1] ) / s;
+        q[2] = 0.25 * s;
+        }
+    }
+}
+
+
+/* quaternion zu matrix */
+
+
+/*TODO: missing defaults*/
+sc_camera_t *sc_camera_new()
+{
+    sc_camera_t *camera = SC_ALLOC (sc_camera_t, 1);
+    return camera;
+} 
+
+void sc_camera_init(sc_camera_t *camera, sc_camera_vec3_t position, 
+    sc_camera_vec4_t orientation, double FOV, int width, int height, 
+    sc_camera_coords_t near, sc_camera_coords_t far)
+{
+    memcpy(camera->position, position, sizeof (camera->position));
+    memcpy(camera->orientation, orientation, sizeof (camera->orientation));
+    camera->FOV = FOV;
+    camera->width = width;
+    camera->height = height;
+    /* maybe assert here that the values are positive */
+    camera->near = near;
+    camera->far = far;
+}
+
+void sc_camera_destroy(sc_camera_t *camera)
+{
+    SC_FREE (camera);
+}
+
+/* TODO hier fehlen noch assertion zu degenerierten fällen */
+void sc_camera_look_at(sc_camera_t *camera, const sc_camera_vec3_t eye, 
+    const sc_camera_vec3_t center, const sc_camera_vec3_t up)
+{
+    
+    memcpy(camera->position, eye, sizeof (camera->position));
+
+    sc_camera_vec3_t z_new;
+    sc_camera_subtract(center, eye, z_new);
+    sc_camera_scalar(1.0/sc_camera_norm(z_new), z_new, z_new);
+
+    sc_camera_vec3_t x_new;
+    sc_camera_cross_prod(up, z_new, x_new);
+    sc_camera_scalar(1.0/sc_camera_norm(x_new), x_new, x_new);
+
+    sc_camera_vec3_t y_new;
+    sc_camera_cross_prod(z_new, x_new, y_new);
+
+    sc_camera_mat3x3_t rotation = {
+        x_new[0], y_new[0], z_new[0],
+        x_new[1], y_new[1], z_new[1],
+        x_new[2], y_new[2], z_new[2]
+    };
+
+    sc_camera_vec4_t q;
+    sc_camera_mat3x3_to_quaternion(rotation, q);
+
+    memcpy(camera->orientation, q, sizeof (camera->orientation));
+}
+
+/* also transformations for single points */
+
+/* lots of other functions possible to move the camera around or zoom in */
+
+void sc_camera_get_view(sc_camera_t *camera, sc_camera_mat4x4_t view_matrix)
+{
+    /* calculate rotation matrix from the quaternion and write it in upper left block*/
+    sc_camera_coords_t xx = camera->orientation[0] * camera->orientation[0];
+    sc_camera_coords_t yy = camera->orientation[1] * camera->orientation[1];
+    sc_camera_coords_t zz = camera->orientation[2] * camera->orientation[2];
+    sc_camera_coords_t wx = camera->orientation[3] * camera->orientation[0];
+    sc_camera_coords_t wy = camera->orientation[3] * camera->orientation[1];
+    sc_camera_coords_t wz = camera->orientation[3] * camera->orientation[2];
+    sc_camera_coords_t xy = camera->orientation[0] * camera->orientation[1];
+    sc_camera_coords_t xz = camera->orientation[0] * camera->orientation[2];
+    sc_camera_coords_t yz = camera->orientation[1] * camera->orientation[2];
+
+    view_matrix[0] = 1.0 - 2.0 * (yy + zz);
+    view_matrix[1] = 2.0 * (xy + wz);
+    view_matrix[2] = 2.0 * (xz - wy);
+    view_matrix[3] = 0.0;
+
+    view_matrix[4] = 2.0 * (xy - wz);
+    view_matrix[5] = 1.0 - 2.0 * (xx + zz);
+    view_matrix[6] = 2.0 * (yz + wx);
+    view_matrix[7] = 0.0;
+
+    view_matrix[8] = 2.0 * (xz + wy);
+    view_matrix[9] = 2.0 * (yz - wx);
+    view_matrix[10] = 1.0 - 2.0 * (xx + yy);
+    view_matrix[11] = 0.0;
+
+    view_matrix[12] = 0.0;
+    view_matrix[13] = 0.0;
+    view_matrix[14] = 0.0;
+    view_matrix[15] = 1.0;
+
+    sc_camera_mat4x4_t translation_matrix = {
+        1., 0., 0., 0.,
+        0., 1., 0., 0.,
+        0., 0., 1., 0.,
+        -camera->position[0], -camera->position[1], -camera->position[2], 1. 
+    };
+
+    sc_camera_mult_4x4_4x4(view_matrix, translation_matrix, view_matrix);
+}
+
+void sc_camera_get_projection(sc_camera_t *camera, sc_camera_mat4x4_t proj_matrix)
+{
+    /* the factor 2 could be reduced */
+    sc_camera_coords_t s_x = 2.0 * camera->near * tan(camera->FOV / 2.0);
+    sc_camera_coords_t s_y = s_x * ((sc_camera_coords_t) camera->height / 
+        (sc_camera_coords_t) camera->width); 
+    sc_camera_coords_t s_z = camera->far - camera->near;
+
+    proj_matrix[0] = 2.0 * camera->near / s_x;
+    proj_matrix[1] = 0.0;
+    proj_matrix[2] = 0.0;
+    proj_matrix[3] = 0.0;
+
+    proj_matrix[4] = 0.0;
+    proj_matrix[5] = 2.0 * camera->near / s_y;
+    proj_matrix[6] = 0.0;
+    proj_matrix[7] = 0.0;
+
+    proj_matrix[8] = 0.0;
+    proj_matrix[9] = 0.0;
+    proj_matrix[10] = (camera->near + camera->far) / s_z;
+    proj_matrix[11] = 1.0;
+
+    proj_matrix[12] = 0.0;
+    proj_matrix[13] = 0.0;
+    proj_matrix[14] = (-2.0 * camera->near * camera->far) / s_z;
+    proj_matrix[15] = 0.0;
+}
+
+/* How the points should be passed to the function below and how to discard points */
+
+/* points of form sc_camera_vec3_t? */
+void sc_camera_view_transform(sc_camera_t *camera, sc_array_t *points_in,
+     sc_array_t *points_out);
+
+/* the idea is to return the indices of points that are in the camera view */
+/* the function does not easily know if the points are already in camera space */
+void sc_camera_clipping_pre(sc_camera_t *camera, sc_array_t *points, 
+    sc_array_t *indices);
+
+/* probably no clipping in this function but what is with points that have z=0? */
+/* transform to canonical view volume [-1,1]^3 */
+/* what is if user wants to only transorm points with certain indices 
+(because he called clipping_pre before)? */
+void sc_camera_projection_transform(sc_camera_t *camera, sc_array_t *points_in, 
+    sc_array_t *points_out);
+
+/* clipping post has nothing to do with the camera itself so the carameter 
+    parameter is not needed */
+void sc_camera_clipping_post(sc_camera_t *camera, sc_array_t *points, 
+    sc_array_t *indices);
+
+/* does all steps in one go (world -> view -> canonical view volume (with clipping) */
+void sc_camera_transform(sc_camera_t *camera, sc_array_t *points_in, 
+    sc_array_t *points_out, sc_array_t *indices);
+
+/* what format should planes have? (currently vec4 = (a,b,c,d) -> ax + by + cz + d = 0) */
+void sc_camera_get_frustum(sc_camera_t *camera, sc_camera_vec4_t near, 
+    sc_camera_vec4_t far, sc_camera_vec4_t left,
+    sc_camera_vec4_t right, sc_camera_vec4_t up, sc_camera_vec4_t down)
+{
+    sc_camera_mat4x4_t view;
+    sc_camera_get_view(camera, view);
+
+    sc_camera_mat4x4_t projection;
+    sc_camera_get_projection(camera, projection);
+
+    /* TODO : maybe i want to use only 2 matrices and not 3 */
+    sc_camera_mat4x4_t transform;
+    sc_camera_mult_4x4_4x4(projection, view, transform);
+
+    print_mat(projection, 4);
+
+    /* 
+    near = (0, 0, -1, -1)
+    far = (0, 0, 1, -1)
+    left = (-1, 0, 0, -1)
+    right = (1, 0, 0, 1)
+    up = (1, 0, 0, -1)
+    down = (-1, 0, 0, -1) 
+    */
+
+    near[0] = 0.;
+    near[1] = 0.;
+    near[2] = -1.;
+    near[3] = -1.;
+
+    far[0] = 0.;
+    far[1] = 0.;
+    far[2] = 1.;
+    far[3] = -1.;
+
+    left[0] = -1.;
+    left[1] = 0.;
+    left[2] = 0.;
+    left[3] = -1.;
+
+    right[0] = 1.;
+    right[1] = 0.;
+    right[2] = 0.;
+    right[3] = 1.;
+
+    up[0] = 1.;
+    up[1] = 0.;
+    up[2] = 0.;
+    up[3] = -1.;
+
+    down[0] = -1.;
+    down[1] = 0.;
+    down[2] = 0.;
+    down[3] = -1.;
+
+    print_mat(transform, 4);
+
+    sc_camera_mult_v4_4x4(near, transform, near);
+    sc_camera_mult_v4_4x4(far, transform, far);
+    sc_camera_mult_v4_4x4(left, transform, left);
+    sc_camera_mult_v4_4x4(right, transform, right);
+    sc_camera_mult_v4_4x4(up, transform, up);
+    sc_camera_mult_v4_4x4(down, transform, down);
+}
+
+void sc_camera_get_frustum_corners(sc_camera_t *camera, sc_camera_vec3_t lbn, 
+    sc_camera_vec3_t rbn, sc_camera_vec3_t ltn, sc_camera_vec3_t rtn, 
+    sc_camera_vec3_t lbf, sc_camera_vec3_t rbf, sc_camera_vec3_t ltf,
+    sc_camera_vec3_t rtf)
+{
+    sc_camera_mat4x4_t view;
+    sc_camera_get_view(camera, view);
+
+    sc_camera_mat4x4_t projection;
+    sc_camera_get_projection(camera, projection);
+
+    /* TODO : maybe i want to use only 2 matrices and not 3 */
+    sc_camera_mat4x4_t inv_transform;
+    sc_camera_mult_4x4_4x4(projection, view, inv_transform);
+    sc_camera_gluInvertMatrix(inv_transform, inv_transform);
+
+    printf("(PV)^-1 : \n");
+    print_mat(inv_transform, 4);
+
+    // sc_camera_vec4_t corners[8];
+
+    // for (size_t i = 0; i < 8; ++i)
+    // {
+
+    // }
+
+    sc_camera_vec4_t    lbn_w = {-1., -1., -1., 1.},  
+                        rbn_w = { 1., -1., -1., 1.},  
+                        ltn_w = {-1.,  1., -1., 1.},  
+                        rtn_w = { 1.,  1., -1., 1.},  
+                        lbf_w = {-1., -1.,  1., 1.},  
+                        rbf_w = { 1., -1.,  1., 1.},  
+                        ltf_w = {-1.,  1.,  1., 1.},  
+                        rtf_w = { 1.,  1.,  1., 1.};
+
+    sc_camera_mult_4x4_v4(inv_transform, lbn_w, lbn_w);
+    sc_camera_mult_4x4_v4(inv_transform, rbn_w, rbn_w);
+    sc_camera_mult_4x4_v4(inv_transform, ltn_w, ltn_w);
+    sc_camera_mult_4x4_v4(inv_transform, rtn_w, rtn_w);
+    sc_camera_mult_4x4_v4(inv_transform, lbf_w, lbf_w);
+    sc_camera_mult_4x4_v4(inv_transform, rbf_w, rbf_w);
+    sc_camera_mult_4x4_v4(inv_transform, ltf_w, ltf_w);
+    sc_camera_mult_4x4_v4(inv_transform, rtf_w, rtf_w);
+
+    sc_camera_scalar(1. / lbn_w[3], lbn_w, lbn);
+    sc_camera_scalar(1. / rbn_w[3], rbn_w, rbn);
+    sc_camera_scalar(1. / ltn_w[3], ltn_w, ltn);
+    sc_camera_scalar(1. / rtn_w[3], rtn_w, rtn);
+    sc_camera_scalar(1. / lbf_w[3], lbf_w, lbf);
+    sc_camera_scalar(1. / rbf_w[3], rbf_w, rbf);
+    sc_camera_scalar(1. / ltf_w[3], ltf_w, ltf);
+    sc_camera_scalar(1. / rtf_w[3], rtf_w, rtf);
+}
--- src/sc_camera.h
+++ src/sc_camera.h
@@ -6,50 +6,55 @@
    y-axis pointing up and x-axis pointing to the right
 */
 
-#include /* sc_array_t */
+#ifndef SC_CAMERA_H
+#define SC_CAMERA_H
 
-typedef double sc_camera_coords_t; /* p4est uses double most compoter graphics (GPU) float*/
+#include <sc_containers.h>/* sc_array_t */
+/* p4est uses double most compoter graphics (GPU) float*/
+typedef double sc_camera_coords_t; 
 
 typedef sc_camera_coords_t sc_camera_vec3_t[3];
 typedef sc_camera_coords_t sc_camera_vec4_t[4];
-
-typedef sc_camera_coords_t sc_camera_mat4x4_t[16]; /* column major for easy to use export? */
+/* column major  */
+typedef sc_camera_coords_t sc_camera_mat4x4_t[16];
+typedef sc_camera_coords_t sc_camera_mat3x3_t[9]; 
 
 typedef struct sc_camera
 {
     /* extern */
     sc_camera_vec3_t position;
 
-    sc_camera_vec4_t orientation; // quaternion
+    sc_camera_vec4_t orientation; /* quaternion */
     /* or 
     * float up[3];
     * float front[3]; 
     */
     
     /* intern */
-    double FOV; /* vertical? */
-    double aspect_ratio; /* width/height */
+    double FOV; /* horizontal */
+    /* double aspect_ratio; */ /* width/height */
+    int width, height; /* Width and Height for the aspect ratio ()*/
 
-    sc_camera_coords_t near, far; /* sign? */
+    sc_camera_coords_t near, far; /* postive values */
 
 } sc_camera_t;
 
+/* matrix multiplikation */ /* could make size variable (TODO: add static) */
+void sc_camera_mult_4x4_v4(const sc_camera_mat4x4_t mat, 
+    const sc_camera_vec4_t vec, sc_camera_vec4_t out);
 
 sc_camera_t *sc_camera_new(); 
 
-void sc_camera_init(sc_camera_vec3_t position, sc_camera_vec4_t orientation, double FOV, 
-    double aspect_ratio, sc_camera_coords_t near, sc_camera_coords_t far);
-
-sc_camera_t *sc_camera_destroy(sc_camera_t *camera);
-
-void sc_camera_set_extern(sc_camera_t *camera, sc_camera_vec3_t position, 
-    sc_camera_vec4_t orientation);
-
-void sc_camera_set_intern(double FOV, double aspect_ratio, 
+void sc_camera_init(sc_camera_t *camera, sc_camera_vec3_t position, 
+    sc_camera_vec4_t orientation, double FOV, int width, int height, 
     sc_camera_coords_t near, sc_camera_coords_t far);
 
-void sc_camera_look_at(sc_camera_t *camera, sc_camera_vec3_t eye, 
-    sc_camera_vec3_t center, sc_camera_vec3_t up);
+void sc_camera_destroy(sc_camera_t *camera);
+
+void sc_camera_look_at(sc_camera_t *camera, const sc_camera_vec3_t eye, 
+    const sc_camera_vec3_t center, const sc_camera_vec3_t up);
+
+/* also transformations for single points */
 
 /* lots of other functions possible to move the camera around or zoom in */
 
@@ -65,16 +70,20 @@ void sc_camera_view_transform(sc_camera_t *camera, sc_array_t *points_in,
 
 /* the idea is to return the indices of points that are in the camera view */
 /* the function does not easily know if the points are already in camera space */
-void sc_camera_clipping_pre(sc_camera_t *camera, sc_array_t *points, sc_array_t *indices);
+void sc_camera_clipping_pre(sc_camera_t *camera, sc_array_t *points, 
+    sc_array_t *indices);
 
 /* probably no clipping in this function but what is with points that have z=0? */
 /* transform to canonical view volume [-1,1]^3 */
-/* what is if user wants to only transorm points with certain indices (because he called clipping_pre before)? */
+/* what is if user wants to only transorm points with certain indices 
+(because he called clipping_pre before)? */
 void sc_camera_projection_transform(sc_camera_t *camera, sc_array_t *points_in, 
     sc_array_t *points_out);
 
-/* clipping post has nothing to do with the camera itself so the carameter parameter is not needed */
-void sc_camera_clipping_post(sc_camera_t *camera, sc_array_t *points, sc_array_t *indices);
+/* clipping post has nothing to do with the camera itself so the carameter 
+    parameter is not needed */
+void sc_camera_clipping_post(sc_camera_t *camera, sc_array_t *points, 
+    sc_array_t *indices);
 
 /* does all steps in one go (world -> view -> canonical view volume (with clipping) */
 void sc_camera_transform(sc_camera_t *camera, sc_array_t *points_in, 
@@ -82,5 +91,21 @@ void sc_camera_transform(sc_camera_t *camera, sc_array_t *points_in,
 
 
 /* what format should planes have? (currently vec4 = (a,b,c,d) -> ax + by + cz + d = 0) */
-void sc_camera_get_frustum(sc_camera_t *camera, vec4 near, vec4 far, vec4 left,
-     vec4 right, vec4 up, vec4 down);
+void sc_camera_get_frustum(sc_camera_t *camera, sc_camera_vec4_t near, 
+    sc_camera_vec4_t far, sc_camera_vec4_t left,
+    sc_camera_vec4_t right, sc_camera_vec4_t up, sc_camera_vec4_t down);
+
+
+void sc_camera_get_frustum_corners(sc_camera_t *camera, sc_camera_vec3_t lbn, 
+    sc_camera_vec3_t rbn, sc_camera_vec3_t ltn, sc_camera_vec3_t rtn, 
+    sc_camera_vec3_t lbf, sc_camera_vec3_t rbf, sc_camera_vec3_t ltf,
+    sc_camera_vec3_t rtf);
+/*
+void sc_camera_set_extern(sc_camera_t *camera, sc_camera_vec3_t position, 
+    sc_camera_vec4_t orientation);
+
+void sc_camera_set_intern(sc_camera_t *camera, double FOV, int width, int height, 
+    sc_camera_coords_t near, sc_camera_coords_t far);
+*/
+
+#endif
